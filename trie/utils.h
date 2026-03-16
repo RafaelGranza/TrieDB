2c53f122ce731bda75250f3493d8b77fa7b8a148
Rafael Granza <rafaelgranzademello@gmail.com>
2026-03-16 13:56:16 -0300

    refactor: Remove old files

--- trie/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef __UTILS_TRIEDB_H__
-#define __UTILS_TRIEDB_H__
-
-#include <string>
-#include <fstream>
-
-void remove_from_file(std::string path, std::string key);
-
-#endif // __UTILS_TRIEDB_H__
